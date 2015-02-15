7aea1a198ca456e209259109a780d231aaba6a3d
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-02-15 16:27:20 +0100

    Read data from stdin

--- aes.py
+++ aes.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
 """
 An implementation of the AES cipher.
 """
 
-import fileinput
+import sys, binascii
+
+KEY_LENGTH = 16 # Cipher key length in bytes
+BLOCK_SIZE = 16 # Block size in bytes
 
 
 if __name__ == '__main__':
-    for line in fileinput.input():
-        print(line)
+    key = sys.stdin.buffer.read(KEY_LENGTH)
+    print(binascii.hexlify(key))
+
+    # sys.stdout.buffer.write(key)
