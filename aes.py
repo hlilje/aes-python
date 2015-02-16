e47c7b968600a592b831ea94fb6b8bb2406b9dc7
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-02-16 13:18:57 +0100

    Implement pseudocode of basic key expansion

--- aes.py
+++ aes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-An implementation of the AES cipher. Reads a binary file from stdin and outputs
-the result on stdout.
+An bit implementation of the AES cipher. Reads a binary file from stdin and
+outputs the result on stdout.
 """
 
 import sys, binascii, rijndael
@@ -48,6 +48,13 @@ if __name__ == '__main__':
     key = bytearray(sys.stdin.buffer.read(KEY_LENGTH)) # Read the cipher key
     plain_text = bytearray(sys.stdin.buffer.read())    # Read the data to be encrypted
     state = [[0] * 4 for i in range(4)]                # Initialise state 4 x 4 byte matrix
+
+    Nb = 4  # Number of columns (32-bit words) comprising the state
+    Nk = 4  # Number of 32-bit words comprising the cipher key
+    Nr = 10 # Number of rounds
+    # key[4*Nk]
+    w = [Nb*(Nr+1)]
+    rijndael.expand_keys(key, w, Nk, Nr)
 
     print(binascii.hexlify(key))
     print(binascii.hexlify(plain_text))
