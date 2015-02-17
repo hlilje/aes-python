5a2edeef0610d4a80eedb41dd9d347da12e57890
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-02-17 09:16:40 +0100

    Implement ShiftRows

--- aes.py
+++ aes.py
@@ -11,6 +11,13 @@ import rijndael
 KEY_LENGTH = 16 # Key length in bytes
 
 
+def rotate(state, steps):
+    """
+    Helpher method to rotate the state steps to the left (positive) or
+    right (negative).
+    """
+    return state[steps:] + state[0:steps]
+
 def add_round_key(state, key_exp, enc_round, offset):
     """
     Combine each byte of the state with a block of the round key using bitwise
@@ -30,12 +37,15 @@ def sub_bytes(state, enc_round, offset):
     # Substitute with Sbox
     for i in range(offset): state[i] = rijndael.sbox[state[i]]
 
-def shift_rows(state):
+def shift_rows(state, nb):
     """
     Perform a transposition step where the last three rows of the state are
     shifted cyclically a certain number of steps.
     """
-    pass
+    # Shift columns increasing steps to the left except the first
+    for i in range(nb):
+        offset = i * nb
+        state[offset:offset+nb] = rotate(state[offset:offset+nb], i)
 
 def mix_columns(state):
     """
@@ -49,18 +59,18 @@ def encrypt(plain_text, key_exp, nb, nr):
     Encrypt the binary data with the given key.
     Return the final state.
     """
-    offset = (nb ** 2)
-    state = [0] * offset           # Initialise state 'matrix'
+    offset = (nb ** 2)                       # 'Matrix' offset
+    state = [0] * offset                     # Initialise state 'matrix'
     add_round_key(state, key_exp, 0, offset) # Initial key round
 
     for i in range(1, nr):
         sub_bytes(state, i, offset)
-        shift_rows(state)
+        shift_rows(state, nb)
         mix_columns(state)
         add_round_key(state, key_exp, i, offset)
 
     sub_bytes(state, i, offset)
-    shift_rows(state)
+    shift_rows(state, nb)
     add_round_key(state, key_exp, nr, offset)
 
     return state
